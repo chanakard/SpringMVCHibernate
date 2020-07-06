885e2d8bb5e5427d359ad77ccaad645170a54c33
chanakard <chanakaroshan86@gmail.com>
2020-07-07 05:08:09 +0530

    Master page created and theme added

--- SpringHibernateApp/src/main/java/com/personnel/springhibernateapp/controller/PageController.java
+++ SpringHibernateApp/src/main/java/com/personnel/springhibernateapp/controller/PageController.java
@@ -12,7 +12,24 @@ public class PageController {
 	@RequestMapping(value = { "/", "/home", "/index" })
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("Page");
-		mv.addObject("greeting", "Welcome to Spring Web MVC");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome",true);
+		return mv;
+	}
+	
+	@RequestMapping("/about")
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("Page");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout",true);
+		return mv;
+	}
+	
+	@RequestMapping("/contact")
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("Page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact",true);
 		return mv;
 	}
 	
---
+++ SpringHibernateApp/src/main/webapp/WEB-INF/view/about.jsp
@@ -0,0 +1,6 @@
+<div class="container">
+	<div class="row">
+	<h1>About Us</h1>
+	<p>This is about us page</p>
+	</div>
+</div>
